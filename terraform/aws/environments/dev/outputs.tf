188051998c7c27ed4caf37891542b31c18ef11f4
Rick Ren <madongchn@163.com>
2026-03-06 06:41:18 +0800

    fix(infra): update outputs to match ec2_public/ec2_private module split

--- terraform/aws/environments/dev/outputs.tf
+++ terraform/aws/environments/dev/outputs.tf
@@ -1,2 +1,5 @@
-output "server_public_ip" { value = module.ec2.public_ip }
-output "vpc_id"           { value = module.vpc.vpc_id }
+output "server1_public_ip"  { value = module.ec2_public.public_ips }
+output "server1_private_ip" { value = module.ec2_public.private_ips }
+output "server2_private_ip" { value = module.ec2_private.private_ips }
+output "vpc_id"             { value = module.vpc.vpc_id }
+output "private_subnet_ids" { value = module.vpc.private_subnet_ids }
